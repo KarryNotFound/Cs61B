274225cffc278ea3cdff515d6988bbafde5a896b
KyMms <1589680745@qq.com>
2024-10-27 19:34:39 +0800

    Day2_2

--- Day1/src/main/java/com/whut/class4/SLList.java
+++ Day1/src/main/java/com/whut/class4/SLList.java
@@ -1,22 +1,61 @@
 package com.whut.class4;
 
 public class SLList {
-    public IntNode first;
 
+    // The first Item, if it exists, is at sentinel.next
+    private  IntNode sentinel;
+    private int size;
     // Create a new SLList with one item, namely x.
     public SLList(int x){
-        first = new IntNode(x,null);
+        sentinel = new IntNode(0,null);
+        sentinel.next = new IntNode(x,null);
+        size = 1;
+    }
+
+    // Create an empty SLList
+    public SLList() {
+        sentinel = new IntNode(0,null);
+        size = 0;
     }
 
     // Adds item x to the front of the list
     public void addFirst(int x){
-        first = new IntNode(x,first);
+        sentinel.next = new IntNode(x,sentinel.next);
+        size += 1;
     }
 
     // Gets the first item in the list
     public int getFirst() {
-        return first.item;
+        return sentinel.next.item;
     }
+
+    // Adds an element in the end of the list
+    public void addLast(int x) {
+        size += 1;
+        IntNode p = sentinel;
+
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new IntNode(x, null);
+
+
+    }
+
+    //Returns the size of the list
+//    public int size() {
+//        return size(first);
+//    }
+        public int size() {
+            return size;
+        }
+
+    // Returns the size of the list, starting at IntNode p
+    private int size(IntNode p) {
+            if(p.next == null) return 1;
+            return 1 + size(p.next);
+    }
+
 
     public static void main(String[] args) {
         SLList L = new SLList(5);
